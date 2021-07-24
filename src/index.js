a556e63efdb6bac22b86bb44439c0fccdcc8e0b6
Christian Reindl <chris.reindl@gmail.com>
2021-07-24 17:23:51 -0500

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,10 +1,27 @@
-import "./styles.css";
+import gsap from "gsap";
+import { ScrollTrigger } from "gsap/all";
+import "./scss/main.scss";
 
-document.getElementById("app").innerHTML = `
-<h1>Hello Vanilla!</h1>
-<div>
-  We use the same configuration as Parcel to bundle this sandbox, you can find more
-  info about Parcel 
-  <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
-</div>
-`;
+gsap.registerPlugin(ScrollTrigger);
+
+const tl = gsap.timeline({
+  scrollTrigger: {
+    trigger: ".featured-image",
+    scrub: true
+  }
+});
+
+tl.fromTo(
+  ".featured-image__inner",
+  {
+    yPercent: -40,
+    height: "140%",
+    ease: "none"
+  },
+  {
+    yPercent: 0,
+    height: "140%",
+    ease: "none"
+  },
+  0
+);
